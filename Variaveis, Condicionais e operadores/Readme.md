d70ffe56991580de3844d3ab9f2a3325b3331014
Caio Ribeiro <caio.ribeiro98@outlook.com>
2021-03-05 19:19:03 -0300

    Adiciona Variaveis, Condicionais e operadores

---
+++ Variaveis, Condicionais e operadores/Readme.md
@@ -0,0 +1,70 @@
+<h1 align="center">
+    <img alt="Launchbase" src="https://storage.googleapis.com/golden-wind/bootcamp-launchbase/logo.png" width="400px" />
+</h1>
+
+<h3 align="center">
+  Desafio 1-1: Primeiros passos com JS
+</h3>
+
+<blockquote align="center">“Querer vencer significa já ter percorrido metade do caminho.”</blockquote>
+
+<p align="center">
+
+  <a href="https://rocketseat.com.br">
+    <img alt="Made by Rocketseat" src="https://img.shields.io/badge/made%20by-Rocketseat-%23F8952D">
+  </a>
+
+  <a href="LICENSE" >
+    <img alt="License" src="https://img.shields.io/badge/license-MIT-%23F8952D">
+  </a>
+
+</p>
+
+<p align="center">
+  <a href="#rocket-sobre-o-desafio">Sobre o desafio</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#calendar-entrega">Entrega</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+## :rocket: Sobre o desafio
+
+Desafios para fortalecer alguns conceitos, entre eles:
+
+- **Variáveis**;
+- **Condicionais**;
+- **Operadores**.
+
+### Cálculo de IMC
+
+Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
+
+Comece criando constantes para armazenar o `nome`, `peso`, `altura` e `sexo` de uma pessoa, por exemplo:
+
+```js
+const nome = "Carlos";
+const peso = 84;
+const altura = 1.88;
+```
+
+A partir desses dados armazene em uma constante chamada `imc` o cálculo do índice de massa corporal definido pela fórmula abaixo:
+
+```js
+peso / (altura * altura);
+```
+
+Baseado no valor obtido através desse cálculo exiba as seguintes mensagens:
+
+- `SE` o `IMC` maior ou igual a `30`: Carlos você está acima do peso;
+- `SE` o `IMC` menor que `29.9`: Carlos você não está acima do peso;
+
+## :calendar: Entrega
+
+Esse desafio **não precisa ser entregue** e não receberá correção, mas você pode ver um exemplo de solução [aqui](https://github.com/Rocketseat/bootcamp-launchbase-desafios-01/tree/codigos). Após concluí-lo, adicionar esse código ao seu Github é uma boa forma de demonstrar seus conhecimentos para oportunidades futuras.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](../LICENSE) para mais detalhes.
+
+---
+
+Feito com :purple_heart: by [Rocketseat](https://rocketseat.com.br) :wave: [Entre na nossa comunidade!](https://discordapp.com/invite/gCRAFhc)
---
+++ Variaveis, Condicionais e operadores/index.js
@@ -0,0 +1,110 @@
+/*      Programa que calcula IMC e imprime em tela o resultado       */
+
+//      Exemplo 1      //
+// const nome = 'Mateus'
+// const peso = 72
+// const altura = 1.70
+
+// const imc = peso / (altura * altura)
+
+// if (imc >= 24.9) {
+//     console.log(`${nome}, seu IMC é ${imc}. Você está acima do peso.`)
+// } else {
+//     console.log(`${nome}, seu IMC é ${imc}. Você não está acima do peso.`)
+// }
+
+// if (imc < 18.5) {
+//     console.log('Classificação: MAGREZA. Nível de obesidade: 0.')
+// } else if (imc < 24.9) {
+//     console.log('Classificação: NORMAL. Nível de obesdade: 0.')
+// } else if (imc < 29.9) {
+//     console.log('Classificação: OBESIDADE. Nível de obesidade: 1.')
+// } else if (imc < 39.9) {
+//     console.log('Classificação: OBESIDADE SEVERA. Nível de obesidade: 2.')
+// } else {
+//     console.log('Classificação: OBESIDADE MÓRBIDA. Nível de obesidade: 3.')
+// }
+
+
+//      Exemplo 2      //
+/*
+const nome = 'Letícia'
+const peso = 65
+const altura = 1.60
+
+const imc = peso / (altura * altura)
+
+if (imc >= 24.9) {
+    console.log(`${nome}, seu IMC é ${imc}. Você está acima do peso.`)
+} else {
+    console.log(`${nome}, seu IMC é ${imc}. Você não está acima do peso.`)
+}
+
+if (imc < 18.5) {
+    console.log('Classificação: MAGREZA. Nível de obesidade: 0.')
+} else if (imc < 24.9) {
+    console.log('Classificação: NORMAL. Nível de obesdade: 0.')
+} else if (imc < 29.9) {
+    console.log('Classificação: OBESIDADE. Nível de obesidade: 1.')
+} else if (imc < 39.9) {
+    console.log('Classificação: OBESIDADE SEVERA. Nível de obesidade: 2.')
+} else {
+    console.log('Classificação: OBESIDADE MÓRBIDA. Nível de obesidade: 3.')
+}
+*/
+
+
+//      Exemplo 3      //
+/*
+const nome = 'Jonathan '
+const peso = 105
+const altura = 1.85
+
+const imc = peso / (altura * altura)
+
+if (imc >= 24.9) {
+    console.log(`${nome}, seu IMC é ${imc}. Você está acima do peso.`)
+} else {
+    console.log(`${nome}, seu IMC é ${imc}. Você não está acima do peso.`)
+}
+
+if (imc < 18.5) {
+    console.log('Classificação: MAGREZA. Nível de obesidade: 0.')
+} else if (imc < 24.9) {
+    console.log('Classificação: NORMAL. Nível de obesdade: 0.')
+} else if (imc < 29.9) {
+    console.log('Classificação: OBESIDADE. Nível de obesidade: 1.')
+} else if (imc < 39.9) {
+    console.log('Classificação: OBESIDADE SEVERA. Nível de obesidade: 2.')
+} else {
+    console.log('Classificação: OBESIDADE MÓRBIDA. Nível de obesidade: 3.')
+}
+*/
+
+
+//      Exemplo 4      //
+/*
+const nome = ' Isabella '
+const peso = 55
+const altura = 1.74
+
+const imc = peso / (altura * altura)
+
+if (imc >= 24.9) {
+    console.log(`${nome}, seu IMC é ${imc}. Você está acima do peso.`)
+} else {
+    console.log(`${nome}, seu IMC é ${imc}. Você não está acima do peso.`)
+}
+
+if (imc < 18.5) {
+    console.log('Classificação: MAGREZA. Nível de obesidade: 0.')
+} else if (imc < 24.9) {
+    console.log('Classificação: NORMAL. Nível de obesdade: 0.')
+} else if (imc < 29.9) {
+    console.log('Classificação: OBESIDADE. Nível de obesidade: 1.')
+} else if (imc < 39.9) {
+    console.log('Classificação: OBESIDADE SEVERA. Nível de obesidade: 2.')
+} else {
+    console.log('Classificação: OBESIDADE MÓRBIDA. Nível de obesidade: 3.')
+}
+*/
